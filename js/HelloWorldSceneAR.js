db901d28dfc40c764843a1d8c98e53adc2611169
Helen Li <hca1314@gmail.com>
2019-05-02 15:01:22 -0500

    able to render 3D objs in ARScene

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -8,7 +8,14 @@ import {
   ViroARScene,
   ViroText,
   ViroConstants,
+  ViroARPlane,
+  ViroBox,
+  Viro3DObject,
+  ViroAmbientLight,
+  ViroNode
 } from 'react-viro';
+
+
 
 export default class HelloWorldSceneAR extends Component {
 
@@ -22,12 +29,36 @@ export default class HelloWorldSceneAR extends Component {
 
     // bind 'this' to functions
     this._onInitialized = this._onInitialized.bind(this);
+    this._onLoadEnd = this._onLoadEnd.bind(this)
+    this._onLoadStart = this._onLoadStart.bind(this)
   }
 
   render() {
+    console.log('IN RENDER!')
     return (
-      <ViroARScene onTrackingUpdated={this._onInitialized} >
-        <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
+      <ViroARScene ref="arscene" onTrackingUpdated={this._onInitialized} >
+                  <ViroNode>
+                  <ViroAmbientLight color="#FFFFFF" />
+                  {/* <Viro3DObject source={require('./res/Eames-chair-DSW.obj')}
+                        resources={[require('./res/teak_B.jpg'),
+                                    require('./res/teak_R.jpg'),
+                                    require('./res/teak_D.jpg')]}
+                        position={[0, 0, 0]}
+                        scale={[.01, .01, .01]}
+                        type='OBJ'
+                        onLoadStart={this._onLoadStart}/> */}
+
+                  <Viro3DObject source={require('./res/object_flowers/object_flowers.vrx')}
+                                resources={[require('./res/object_flowers/object_flowers_diffuse.png'),
+                                            require('./res/object_flowers/object_flowers_normal.png'),
+                                            require('./res/object_flowers/object_flowers_specular.png')]}
+                                position={[0, 0, 0]}
+                                scale={[.5, .5, .5]}
+                                type="VRX"/>
+                  </ViroNode>
+          
+          
+                        
       </ViroARScene>
     );
   }
@@ -41,7 +72,23 @@ export default class HelloWorldSceneAR extends Component {
       // Handle loss of tracking
     }
   }
+
+  _onLoadStart() {
+    console.log('STARTED LOADING!')
+  }
+
+  _onLoadEnd() {
+    this.refs["arscene"].getCameraOrientationAsync().then(orientation => {
+      console.log('got camera orientation!', orientation)
+      this.refs["arscene"].performARHitTestWithRay(orientation.forward).then(results => {
+        console.log('hit test results!', results)
+      })
+    })
+  }
 }
+
+
+
 
 var styles = StyleSheet.create({
   helloWorldTextStyle: {
