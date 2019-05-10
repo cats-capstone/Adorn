d279505379d67e0a9c7002bbe4492f7679d0f8a3
Janelle Henney <janellehenney@gmail.com>
2019-05-10 10:59:41 -0500

    sign in, continue as guest and furniture cards

--- js/components/Products.js
+++ js/components/Products.js
@@ -10,7 +10,11 @@ import {
   Title,
   CardItem,
   Icon,
+  Left,
+  Right,
+  Image,
 } from 'native-base';
+import { StyleSheet } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { connect } from 'react-redux';
 import { fetchAllItems } from '../store/2Ditems';
@@ -18,14 +22,15 @@ import { fetchAllItems } from '../store/2Ditems';
 class Products extends Component {
   componentDidMount() {
     this.props.fetchInitialItems();
-    console.log('COMPONENT DID MOUNT');
   }
 
   render() {
     console.log('THIS IS THE STATE', this.props.allItems);
+    allItems = this.props.allItems;
     return (
       <Container>
         <Header>
+          {/* <Left>
           <Button transparent>
             <Icon
               name="ios-arrow-back"
@@ -34,26 +39,37 @@ class Products extends Component {
               }}
             />
           </Button>
+          </Left> */}
           <Body>
             <Title>Furniture</Title>
           </Body>
-          <Button transparent>
-            <Icon name="ios-menu" />
-          </Button>
+          <Right>
+            <Button transparent>
+              <Icon name="ios-menu" />
+            </Button>
+          </Right>
         </Header>
         <Content padder>
-          <Card>
-            <CardItem>
-              <Body>
-                <Text>
-                  Do you like me? Click the icon to add me to your room.
-                </Text>
-                <Button transparent>
-                  <Icon name="ios-add-circle-outline" />
-                </Button>
-              </Body>
-            </CardItem>
-          </Card>
+          {allItems.map(item => (
+            <Card>
+              <CardItem key={item.Name}>
+                <Body>
+                  {/* <Image>{item.ImageUrl}</Image> */}
+                  <Text>{item.Name}</Text>
+                  <Text>${item.Price}</Text>
+                  <Button transparent>
+                    <Icon
+                      name="ios-add-circle-outline"
+                      style={localStyles.icons}
+                    />
+                  </Button>
+                  <Button transparent>
+                    <Icon name="ios-heart-empty" style={localStyles.icons} />
+                  </Button>
+                </Body>
+              </CardItem>
+            </Card>
+          ))}
         </Content>
       </Container>
     );
@@ -76,3 +92,9 @@ export default connect(
   mapState,
   mapDispatch
 )(Products);
+
+const localStyles = StyleSheet.create({
+  icons: {
+    fontSize: 25,
+  },
+});
