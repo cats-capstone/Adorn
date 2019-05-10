399997730c7d330f77e0e79b28f8dab3fd1c5707
Janelle Henney <janellehenney@gmail.com>
2019-05-10 11:24:54 -0500

    other stuff

--- js/components/Products.js
+++ js/components/Products.js
@@ -14,6 +14,7 @@ import {
   Right,
   Image,
 } from 'native-base';
+import { DrawerNavigator } from 'react-navigation';
 import { StyleSheet } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { connect } from 'react-redux';
