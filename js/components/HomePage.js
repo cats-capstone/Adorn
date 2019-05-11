ca708e7d5a1b4a8fce8a33c536028cc7abcd96af
Helen Li <hca1314@gmail.com>
2019-05-10 23:02:10 -0500

    added firebase auth; able to create new user/sign up

--- js/components/HomePage.js
+++ js/components/HomePage.js
@@ -12,24 +12,73 @@ import {
   Label,
 } from 'native-base';
 import { Actions } from 'react-native-router-flux';
+import { auth } from '../../firebase'
 
 export default class HomePage extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      status: 'sign up',
+      email: '',
+      password: ''
+    }
+
+    this.submit = this.submit.bind(this)
+  }
+
+  submit() {
+    console.log('JUST HIT SUBMIT BUTTON!')
+    if (this.state.status === 'sign up') {
+      auth.createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .catch(function(error) {
+          console.log(error.message)
+      })
+    } else {
+      auth.signInWithEmailAndPassword(this.state.email, this.state.password).catch(function(error) {
+        console.log(error.message)
+      })
+    }
+  }
+
   render() {
+    console.log('STATUS: ', this.state.status )
     return (
       <Container>
         <Content>
           <View style={localStyles.content}>
             <Form>
               <Item floatingLabel>
-                <Label>Username</Label>
-                <Input />
+                <Label>Email</Label>
+                <Input
+                  name="email"
+                  value={this.state.email}
+                  onChangeText={text => this.setState({email: text})} />
               </Item>
               <Item floatingLabel last>
                 <Label>Password</Label>
-                <Input />
+                <Input
+                  name="password"
+                  value={this.state.password}
+                  onChangeText={text => this.setState({password: text})} />
               </Item>
             </Form>
-            <Button block style={localStyles.buttons}>
+
+            <Button block style={localStyles.buttons}
+                    onPress={this.submit}>
+              <Text>Submit</Text>
+            </Button>
+
+            <Button block style={localStyles.buttons}
+                    onPress={()=>{
+                      this.setState({status: 'sign up'})
+                    }}>
+              <Text>Sign Up</Text>
+            </Button>
+            <Button block style={localStyles.buttons}
+                    onPress={()=>{
+                      this.setState({status: 'sign in'})
+                    }}>
               <Text>Sign In</Text>
             </Button>
             <Button
