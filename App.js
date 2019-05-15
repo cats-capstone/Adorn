e047d7d588821975b4e0862d989d8e48cf6c254a
Barry <gnyrrab@gmail.com>
2019-05-15 16:13:52 -0500

    yellowbox disabled

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import Routes from './js/components/Routes';
 import { Root } from 'native-base';
 import { Provider } from 'react-redux';
 import store from './js/store/index';
-
+console.disableYellowBox = true
 export default class App extends React.Component {
   render() {
     return (
